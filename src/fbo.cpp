1870c22b3b3af7af917c836336d8deb1c2d9fd11
AntonHakansson <anton.hakansson98@gmail.com>
2021-05-21 06:42:37 +0200

    Windows fix build

--- src/fbo.cpp
+++ src/fbo.cpp
@@ -59,10 +59,9 @@ void FboInfo::resize(int w, int h) {
                              colorTextureTargets[i], 0);
     }
 
-    std::array<GLenum, 8> attachments(
-        {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2, GL_COLOR_ATTACHMENT3,
+    std::array<GLenum, 8> attachments({GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2, GL_COLOR_ATTACHMENT3,
          GL_COLOR_ATTACHMENT4, GL_COLOR_ATTACHMENT5, GL_COLOR_ATTACHMENT6, GL_COLOR_ATTACHMENT7});
-    glDrawBuffers(int(colorTextureTargets.size()), attachments.begin());
+    glDrawBuffers(int(colorTextureTargets.size()), &attachments[0]);
 
     // bind the texture as depth attachment (to the currently bound framebuffer)
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthBuffer, 0);
