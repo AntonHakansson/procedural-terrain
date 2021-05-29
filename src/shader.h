c0acd80d4bc5d2647edc06329f763507bb4a693b
AntonHakansson <anton.hakansson98@gmail.com>
2021-05-29 04:29:51 +0200

    Added glsl preprocessor step

--- src/shader.h
+++ src/shader.h
@@ -3,11 +3,14 @@
 #include <glad/glad.h>
 
 #include <array>
+#include <fstream>
+#include <filesystem>
+#include <iostream>
 
 #include "gpu.h"
 
 struct ShaderInput {
-  std::string filepath;
+  std::filesystem::path filepath;
   /**
    * Describes what type of shader this is
    * - GL_VERTEX_SHADER
@@ -18,6 +21,43 @@ struct ShaderInput {
   GLenum type;
 };
 
+static bool readShaderSource(const std::filesystem::path& filepath, std::ifstream& in, std::stringstream& out, int level) {
+  std::string line;
+  std::string include_keyword = "#include ";
+  auto line_number = 0;
+  while (std::getline(in, line)) {
+    if (line.find(include_keyword, 0) == 0) {
+      auto end = line.find_last_of('\"');
+      if (end == std::string::npos) {
+        std::cout << filepath << ":" << line_number << ": GLSL: Invalid include format: " << line << "\n";
+        return false;
+      }
+      auto include_file = line.substr(include_keyword.size() + 1, end - include_keyword.size() - 1);
+
+      std::filesystem::path include_filepath = filepath.parent_path() / include_file;
+      std::ifstream s_file(include_filepath);
+      if (!s_file.is_open()) {
+        std::cout << filepath << ": GLSL: Could not find include file " << include_filepath << "\n";
+        return false;
+      }
+
+      {
+        out << "#line 0 " << level + 1 << "\n";
+        auto success = readShaderSource(include_filepath, s_file, out, level + 1);
+        if (!success) { return false; }
+      }
+
+      line_number += 2;
+      out << "#line " << line_number << " " << level << "\n";
+      continue;
+    }
+    out << line << "\n";
+    line_number += 1;
+  }
+
+  return true;
+}
+
 template <auto N>
 GLuint loadShaderProgram(const std::array<ShaderInput, N>& shaders, bool allow_errors) {
   GLuint gl_program = glCreateProgram();
@@ -26,11 +66,19 @@ GLuint loadShaderProgram(const std::array<ShaderInput, N>& shaders, bool allow_e
     GLuint gl_shader = glCreateShader(s.type);
 
     std::ifstream s_file(s.filepath);
-    std::string s_src((std::istreambuf_iterator<char>(s_file)), std::istreambuf_iterator<char>());
+    std::stringstream shader_source;
+    auto success = readShaderSource(s.filepath, s_file, shader_source, 0);
+    if (!success) {
+      if (!allow_errors) {
+          gpu::fatal_error("Preprocessor error", s.filepath);
+      }
+      return 0;
+    }
 
-    const char* source = s_src.c_str();
+    std::string full_source{std::istreambuf_iterator<char>(shader_source), std::istreambuf_iterator<char>()};
+    const char *c_str = full_source.c_str();
+    glShaderSource(gl_shader, 1, &c_str, nullptr);
 
-    glShaderSource(gl_shader, 1, &source, nullptr);
 
     glCompileShader(gl_shader);
 
